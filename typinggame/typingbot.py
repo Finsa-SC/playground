e604912746822353377ffc249aaaf729897c0090
Finsa-SC <finsakusumaputra@gmail.com>
2026-06-25 10:19:27 +0700

    feat(typingbot): add panic mechanic for mor humanity

--- typinggame/typingbot.py
+++ typinggame/typingbot.py
@@ -9,9 +9,9 @@ class Enemy(Enum):
     John = {
         "desc": "John is a villager",
         "wpm": 18,
-        "accuracy": 72,
+        "accuracy": 91,
         "start_delay": (1.8, 3.2),
-        "panic": 2
+        "panic_multiplier": 0.35,
     }
 
     Marlo = {
@@ -19,7 +19,7 @@ class Enemy(Enum):
         "wpm": 28,
         "accuracy": 76,
         "start_delay": (1.3, 2.4),
-        "panic": 2
+        "panic_multiplier": 0.29,
     }
 
     Juliana = {
@@ -27,7 +27,7 @@ class Enemy(Enum):
         "wpm": 42,
         "accuracy": 82,
         "start_delay": (1.02, 1.88),
-        "panic": 2
+        "panic_multiplier": 0.24,
     }
 
     Edward = {
@@ -35,7 +35,7 @@ class Enemy(Enum):
         "wpm": 63,
         "accuracy": 90,
         "start_delay": (0.82, 1.48),
-        "panic": 2
+        "panic_multiplier": 0.18,
     }
 
     Bastian = {
@@ -43,7 +43,7 @@ class Enemy(Enum):
         "wpm": 92,
         "accuracy": 95,
         "start_delay": (0.42, 0.88),
-        "panic": 2
+        "panic_multiplier": 0.12,
     }
 
     Finn = {
@@ -51,15 +51,16 @@ class Enemy(Enum):
         "wpm": 125,
         "accuracy": 98,
         "start_delay": (0.18, 0.8),
-        "panic": 2
+        "panic_multiplier": 0.05,
     }
 
 class TypingBot:
     def __init__(self):
         self.opp_name = ""
-        self.opp_wpm = 0
-        self.opp_accuracy = 0
+        self.opp_wpm = 0.0
+        self.opp_accuracy = 0.0
         self.opp_start_delay = (0, 0)
+        self.opp_panic_multiplier = 0.0
 
     def select_bot(self):
         print("Select your opponent")
@@ -70,13 +71,14 @@ class TypingBot:
         if str_opp_num.isdigit():
             opp_num = int(str_opp_num)
 
-            print(len(Enemy))
+            ## Set opponent attribute
             if 1 <= opp_num <= len(Enemy):
                 opponent = list(Enemy)[opp_num - 1]
                 self.opp_name = opponent.name
                 self.opp_wpm = opponent.value['wpm']
                 self.opp_accuracy = opponent.value['accuracy']
                 self.opp_start_delay = opponent.value['start_delay']
+                self.opp_panic_multiplier = opponent.value['panic_multiplier']
             else:
                 raise IndexError("Your input is out of range")
         else:
@@ -85,24 +87,56 @@ class TypingBot:
     def bot_typing(self, word: str):
         word = list(word)
         word_len = len(word)
-        current_delay = 60 / (self.opp_wpm * 5)
+        base_delay = 60 / (self.opp_wpm * 5)
 
         typed = []
         sleep(random.uniform(*self.opp_start_delay))
         for i in range(0, word_len):
             print("\033[H\033[J", end="")
+            print(self.__str__())
+            current_delay = base_delay
+
+            # Rng for panic
+            if self.getting_panic(word):
+                current_delay *= (1 + self.opp_panic_multiplier)
+
+            # rng for accuracy
             if self.accuracy():
                 typed.append(word[i])
             else:
                 typed.append(random.choice(string.ascii_letters))
+
             print("".join(typed))
             sleep(current_delay)
 
     def accuracy(self) -> bool:
         return self.opp_accuracy >= random.uniform(1, 100)
 
+    def getting_panic(self, word):
+        chance = min(
+            self.opp_panic_multiplier * (len(word) / 10),
+            0.95
+        )
+        panic = chance > random.random()
+        if panic:
+            self.opp_panic_multiplier = min(
+                self.opp_panic_multiplier + 0.03,
+                0.95
+            )
+
+            if random.random() >= 0.5:
+                penalty = random.uniform(1, 3)
+            else:
+                penalty = random.uniform(1, 6)
+            self.opp_accuracy = max(
+                self.opp_accuracy - penalty,
+                0
+            )
+
+        return panic
+
     def __str__(self):
-        return f"Your opponent is {self.opp_name}, {self.opp_wpm} wpm with {self.opp_accuracy}% accuracy"
+        return f"Your opponent is {self.opp_name}, \n{self.opp_wpm} wpm \nwith {self.opp_accuracy}% accuracy\n{self.opp_panic_multiplier} panic"
 
 if __name__ == "__main__":
     typing = TypingBot()
