272498b7aff805c74f55551303069701d032f690
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-01 10:59:00 +0700

    learn input validation

--- learn_bash/calculator.sh
+++ learn_bash/calculator.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/bash
 
+#Input validation
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+  echo "Missing argument detected!"
+  exit 0
+elif [[ $1 =~ ^[0-9]*$ ]]; then
+  echo "Invalid number, please input digit only"
+  exit 0
+fi
+
+# Input value when call this script [./calculator 2 + 3]
 if [[ $2 == "+" ]]; then
   echo $(( $1 + $3 ))
 elif [[ $2 == "-" ]]; then
@@ -10,10 +20,10 @@ elif [[ $2 == "/" || $2 == ":" ]]; then
   if (( $3 != 0 )); then
     echo $(( $1 / $3 ))
   else
-    echo "Can't devine with zero"
+    echo "Can't divide with zero"
   fi
 elif [[ $2 == "xx" ]]; then
-  echo $(( $1**$3))
+  echo $(( $1**$3 ))
 else
   echo "invalid operation input"
 fi
