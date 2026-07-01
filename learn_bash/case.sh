31216ca614235c5d36c557b643835f0ba81650fb
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-01 13:45:28 +0700

    refactor(case) change asterisk to addition symbol to set minimal one number

--- learn_bash/case.sh
+++ learn_bash/case.sh
@@ -3,7 +3,7 @@
 if [[ -z $1 || -z $2 || -z $3 ]]; then
   echo "Missing argument"
   exit 1
-elif [[ ! $1 =~ ^[0-9]*$ || ! $3 =~ ^[0-9]*$ ]]; then
+elif [[ ! $1 =~ ^[0-9]+$ || ! $3 =~ ^[0-9]+$ ]]; then
   echo "Invalid input, please input digit"
   exit 1
 fi
