485bb9717ba28ca07e2f4f2015f31f9abcd4b9eb
Finsa-SC <finsakusumaputra@gmail.com>
2026-07-01 13:33:35 +0700

    learn case method

---
+++ learn_bash/case.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+if [[ -z $1 || -z $2 || -z $3 ]]; then
+  echo "Missing argument"
+  exit 1
+elif [[ ! $1 =~ ^[0-9]*$ || ! $3 =~ ^[0-9]*$ ]]; then
+  echo "Invalid input, please input digit"
+  exit 1
+fi
+
+case $2 in
+  "+")
+    echo $(( $1 + $3 ))
+    ;;
+  "-")
+    echo $(( $1 - $3 ))
+    ;;
+  "x")
+    echo $(( $1 * $3 ))
+    ;;
+  "/")
+    if (( $3 != 0 )); then
+      echo $(( $1 / $3 ))
+    else
+      echo "Cannot divide with zero"
+    fi
+    ;;
+  *)
+    echo "Invalid operator"
+esac
